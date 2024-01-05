0aa1c1994b763f7c70365f351f631928519cfbd1
Sark-o07 <kosinick01@gmail.com>
2024-01-04 16:49:54 -0800

    fixing bugs

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Exports the TODO list info for a given employee Id to CSV format."""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
@@ -13,6 +13,6 @@ if __name__ == "__main__":
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [witer.writerow(
+        [writer.writerow(
             [user_id, username, t.get("completed"), t.get("title")]
          ) for t in todos]
