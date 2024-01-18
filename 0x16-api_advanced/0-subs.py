c81ddb74f5d8d96c883e4945f63a5f685e1c7f26
Sark-o07 <kosinick01@gmail.com>
2024-01-17 17:27:08 -0800

    how many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,19 +12,16 @@ def number_of_subscribers(subreddit):
     (not active users, total subscribers) for a given subreddit.
     """
 
-    if subreddit is None or  not isinstance(subreddit, str):
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    headers = {'user-agent': 'Google Chrome Version 120.0.6099.234'}
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=headers, allow_redirects=False)
-    
+    response = get(url, headers=user_agent)
+    results = response.json()
 
-    print("response: {}".format(response))
-    if response.status_code != 200:
-        return 0
     try:
-        results = response.json()
         return results.get('data').get('subscribers')
+
     except Exception:
         return 0
