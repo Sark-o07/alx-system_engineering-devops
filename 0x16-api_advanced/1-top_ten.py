d4abc9c9c293c362484f8d08945f9a9e47dae076
Sark-o07 <kosinick01@gmail.com>
2024-01-17 17:25:47 -0800

    top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 """
-prints the title of Top ten hot posts.
+prints the titles of the first 10 hot posts listed for a given subreddit
 """
 
 from requests import get
@@ -15,14 +16,14 @@ def top_ten(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
 
-    headers = {'User-agent': 'Google Chrome Version 120.0.6099.234'}
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-    response = get(url, headers=headers, params=params, allow_redirects=False)
+    response = get(url, headers=user_agent, params=params)
+    results = response.json()
 
     try:
-        results = response.json()
         my_data = results.get('data').get('children')
 
         for i in my_data:
